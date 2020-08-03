a3f7d6ef206cdf6f94fd32c97b0c8f918b3f7b4b
raymag <prayk11@outlook.com>
2020-08-03 11:48:15 -0300

    feat: add Watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,23 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
 async function robot(content){
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -51,6 +64,38 @@ async function robot(content){
         })
         
     }
+}
+
+function limitMaximumSentences(content){
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+}
+
+async function fetchKeywordsOfAllSentences(content){
+    for (const sentence of  content.sentences){
+        sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+}
+
+async function fetchWatsonAndReturnKeywords(sentence){
+    return new Promise((resolve, reject) => {
+        nlu.analyze(
+            {
+              text: sentence,
+              features: {
+                keywords: {}
+              }
+        })
+        .then(response => {
+            const keywords = response.result.keywords.map((keyword) => {
+                return keyword.text
+            })
+
+            resolve(keywords)
+        })
+        .catch(err => {
+            console.log('error: ', err);
+        });
+    })
 }
 
 module.exports = robot
